ce76c80e3efc15aa3fcfabfcfd468d7c97d0d845
Artem Goncharuk <artemgoncharuk@speaktoit.com>
2016-06-28 12:41:29 -0700

    Logs

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ restService.use(bodyParser.json());
 restService.post('/hook', function (req, res) {
 
     console.log('hook request');
-    console.log(req.body.toString());
+    console.log(JSON.stringify(req.body);
 
     try {
         var speech = 'empty speech';
